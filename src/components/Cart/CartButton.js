8cd5bac51f705c4a67efd8da315922a2c2d219da
mishraom12345 <mishrahariom525@gmail.com>
2023-03-01 16:22:18 +0530

    giving functionality to cart btn to show cart item

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
 import classes from './CartButton.module.css';
-
+import { useDispatch } from 'react-redux';
+import { uiAction } from '../../store/UiSlice';
 const CartButton = (props) => {
+  const dispatch = useDispatch()
+  const toggleHandler = ()=>{
+    dispatch(uiAction.toggle())
+
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
